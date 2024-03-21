643770073c0241277baf629c27aae4f1158c9db5
hamzahdili2001 <hdilihamza955@gmail.com>
2024-03-21 00:25:41 +0000

    [add] 3rd api to the home page

--- frontend/src/components/AppRecipeCards.vue
+++ frontend/src/components/AppRecipeCards.vue
@@ -3,20 +3,21 @@
     <div class="mt-10 mb-10 mx-15">
         <h1>Hand-Picked Recipes</h1>
         <div class="d-flex justify-center ga-10 align-center flex-wrap mt-8">
-            <v-col v-for="card of cardsInfo" :key="card" class="pa-0">
-                <v-card :loading="loading" class="mx-auto my-2" width="100%">
+            <v-col v-if="recipes.length > 0" v-for="recipe of recipes" :key="recipe.id" class="pa-0"
+                style="min-width: 322px;">
+                <v-card :loading="loading" class="my-2">
                     <template v-slot:loader="{ isActive }">
                         <v-progress-linear :active="isActive" color="deep-purple" height="4"
                             indeterminate></v-progress-linear>
                     </template>
 
-                    <v-img height="250" :src="card.image" cover></v-img>
+                    <v-img height="250" :src="recipe.image" cover></v-img>
 
                     <v-card-item>
-                        <v-card-title>Cafe Badilico</v-card-title>
+                        <v-card-title>{{ recipe.title }}</v-card-title>
 
                         <v-card-subtitle>
-                            <span class="me-1">Local Favorite</span>
+                            <span class="me-1">Ready in {{ recipe.readyInMinutes }} minutes</span>
 
                             <v-icon color="error" icon="mdi-fire-circle" size="small"></v-icon>
                         </v-card-subtitle>
@@ -24,86 +25,104 @@
 
                     <v-card-text>
                         <v-row align="center" class="mx-0">
-                            <v-rating :model-value="4.5" color="amber" density="compact" size="small" half-increments
-                                readonly></v-rating>
+                            <v-rating :model-value="convertToRating(recipe.spoonacularScore)" color="amber"
+                                density="compact" size="small" half-increments readonly></v-rating>
 
                             <div class="text-grey ms-4">
-                                4.5 (413)
+                                {{ convertToRating(recipe.spoonacularScore) }}
                             </div>
-                        </v-row>
 
-                        <div class="my-4 text-subtitle-1">
-                            $ • Italian, Cafe
+                        </v-row>
+                        <div class="my-4 text-subtitle-1" v-if="recipe.cuisines.length > 0">
+                            <v-icon size="medium" class="mb-1 text-grey">mdi-chef-hat</v-icon> {{
+                recipe.cuisines.join(', ') }}
+                        </div>
+                        <div class="my-4 text-subtitle-1" v-else>
+                            <v-icon size="medium" class="mb-1 text-grey">mdi-chef-hat</v-icon> Global
                         </div>
 
-                        <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio
-                            seating.
+                        <div class="my-4">{{ truncateSummary(recipe.summary, 200) }}
                         </div>
                     </v-card-text>
 
-                    <v-divider class="mx-4 mb-1"></v-divider>
-
-                    <v-card-title>Tonight's availability</v-card-title>
-
-                    <div class="px-4">
-                        <v-chip-group v-model="selection">
-                            <v-chip>5:30PM</v-chip>
-
-                            <v-chip>7:30PM</v-chip>
-
-                            <v-chip>8:00PM</v-chip>
-
-                            <v-chip>9:00PM</v-chip>
-                        </v-chip-group>
-                    </div>
-
                     <v-card-actions>
                         <v-btn color="deep-purple-lighten-2" variant="text" @click="reserve">
-                            Reserve
+                            READ MORE
                         </v-btn>
                     </v-card-actions>
                 </v-card>
             </v-col>
+            <v-col v-else class="text-center p-15 text-grey">
+                <h1>No Recipe Found</h1>
+                <p>Maybe you need an API Key, Go to <a href="https: //spoonacular.com/"
+                        target="_blank">spoonacular.com</a> to get
+                    it.
+                </p>
+            </v-col>
         </div>
-        <div class="d-flex justify-center mt-8">
-            <v-btn style="border: 2px solid #000;" class="rounded-0 text-center" elevation="0">Load More</v-btn>
+        <div class="d-flex justify-center mt-8" v-if="recipes.length > 0">
+            <v-btn style="border: 2px solid #000;" @click="loadMoreRecipes" class="rounded-0 text-center"
+                elevation="0">Load
+                More</v-btn>
         </div>
     </div>
 </template>
 <script>
+import axios from 'axios';
 export default {
     data: () => ({
         loading: false,
         selection: 1,
-        cardsInfo: [
-            {
-                image: "https://www.homemademastery.com/wp-content/uploads/2020/09/IMG_0909.jpg",
-            },
-            {
-                image: "https://www.simplyrecipes.com/thmb/zSvZNZj70cA9uzOgV3oZEV5dvrQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Baklava-LEAD-4-a9d125c8d66547ef9f92c6564a5d5241.jpg",
-            },
-            {
-                image: "https://www.simplyrecipes.com/thmb/KRw_r32s4gQeOX-d07NWY1OlOFk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1c-c2b1885d27d4481c9cfe6f6286a64342.jpg",
-            },
-            {
-                image: "https://assets.bonappetit.com/photos/61b775620fb3fcc4cbf036c1/1:1/w_1920,c_limit/20211208%20Spaghetti%20Squash%20with%20Tomato%20Sauce%20and%20Mozarella%20LEDE.jpg",
-            },
-            {
-                image: "https://www.foodandwine.com/thmb/fVmYbaQzXCz1Prx8VxrW9sMcjMU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Braciole-FT-RECIPE1122-66acf49cef0e4390bec780945709e7f3.jpg",
-            },
-            {
-                image: "https://www.inspiredtaste.net/wp-content/uploads/2023/01/Easy-Bean-Salad-Recipe-Video.jpg",
-            },
-
-        ]
+        loadCount: 12,
+        recipes: []
     }),
-
+    created() {
+        this.getRecipes()
+    },
     methods: {
+
+        loadMoreRecipes() {
+            this.loadCount += 3;
+            this.getRecipes();
+        },
+
+        getRecipes() {
+            const apiKey = "API_KEY" // Get your API key from https://spoonacular.com/
+            const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?number=${this.loadCount}&addRecipeInformation=true&apiKey=${apiKey}`
+            axios.get(apiUrl)
+                .then(response => {
+                    this.recipes = response.data.results;
+                    console.log(this.recipes[ 0 ])
+                })
+                .catch(error => {
+                    console.log("Error:", error)
+                })
+
+
+        },
+        convertToRating(score) {
+
+            return (parseInt(score) / 20).toFixed(1)
+        },
         reserve() {
             this.loading = true
 
             setTimeout(() => (this.loading = false), 2000)
         },
+
+        truncateSummary(summary, maxLength) {
+            const parser = new DOMParser();
+            const parsedHTML = parser.parseFromString(summary, 'text/html');
+            const textContent = parsedHTML.body.textContent.trim();
+
+            if (textContent.length > maxLength) {
+                return textContent.substring(0, maxLength) + '...';
+            } else {
+                return textContent;
+            }
+
+        }
+
     },
 }
 </script>
