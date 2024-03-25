c36849944fdcb10eb48524d414713c94f7aa6e0a
hamzahdili2001 <hdilihamza955@gmail.com>
2024-03-25 21:48:36 +0000

    [add] login user + error handling + responsive login/register form

--- frontend/src/components/AppLogin.vue
+++ frontend/src/components/AppLogin.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="text-center">
-    <v-overlay v-model="appStore.overlay" class="d-flex justify-center align-center">
+  <div class="text-center" style="overflow-y: auto;">
+    <v-overlay v-model="appStore.overlay" class="d-flex justify-center align-center app-overlay">
       <v-btn @click="appStore.overlay = false" icon="mdi-close" variant="plain" text
         class="app-login-close-icon text-white"></v-btn>
-      <div style="width: 650px; max-width: 500px; min-width: 320px;" v-show="appStore.LoginRegister">
-        <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="648" rounded="lg">
+      <div id='login' style="width: 650px; max-width: 500px; min-width: 320px;" v-show="appStore.LoginRegister">
+        <v-card class="mx-auto pa-12 pb-8 login-card" elevation="8" max-width="648" rounded="lg">
           <h3 class="mx-auto mb-8 font-weight-medium text-center" max-width="100%"
             style="font-family: 'Smooch', cursive; font-size: 50px; color: #000000;">Recipy
           </h3>
 
           <div class="text-subtitle-1 text-medium-emphasis">Account</div>
 
-          <v-text-field density="compact" placeholder="Email address" prepend-inner-icon="mdi-email-outline"
-            variant="outlined"></v-text-field>
+          <v-text-field density="compact" v-model="cleanLoginData.username" placeholder="Email address or username"
+            prepend-inner-icon="mdi-email-outline" variant="outlined"></v-text-field>
 
           <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
             Password
@@ -21,19 +21,12 @@
               Forgot login password?</a>
           </div>
 
-          <v-text-field :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'" :type="visible ? 'text' : 'password'"
-            density="compact" placeholder="Enter your password" prepend-inner-icon="mdi-lock-outline" variant="outlined"
+          <v-text-field v-model="cleanLoginData.password" :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+            :type="visible ? 'text' : 'password'" density="compact" placeholder="Enter your password"
+            prepend-inner-icon="mdi-lock-outline" variant="outlined"
             @click:append-inner="visible = !visible"></v-text-field>
 
-          <!-- <v-card class="mb-12" color="surface-variant" variant="tonal">
-                        <v-card-text class="text-medium-emphasis text-caption">
-                            Warning: After 3 consecutive failed login attempts, you account will be temporarily
-                            locked for three hours. If you must login now, you can also click "Forgot login
-                            password?" below to reset the login password.
-                        </v-card-text>
-                    </v-card> -->
-
-          <v-btn class="mb-8" color="blue" size="large" variant="tonal" block>
+          <v-btn @click="login" class="mb-8" color="blue" size="large" variant="tonal" block>
             Log In
           </v-btn>
 
@@ -46,16 +39,18 @@
         </v-card>
       </div>
 
-      <div style="width: 750px; max-width: 700px; min-width: 320px;" v-show="!appStore.LoginRegister">
+      <div id="register" style="height: 100%; width: 750px; max-width: 700px; min-width: 320px; overflow-y: auto;"
+        v-show="!appStore.LoginRegister">
         <form @submit.prevent="register">
-          <v-card class="mx-auto  pa-12 pb-8" elevation="8" max-width="748" rounded="lg">
+          <v-card class="mx-auto h-100 pa-12 pb-8 register-card" style="overflow-y:auto;" elevation="8" max-width="748"
+            rounded="lg">
             <v-row>
-              <v-col cols="6">
+              <v-col cols="12" sm="6">
                 <div class="text-subtitle-1 text-medium-emphasis">First Name</div>
                 <v-text-field density="compact" v-model="registerFormData.first_name" placeholder="First Name"
                   prepend-inner-icon="mdi-account-outline" variant="outlined"></v-text-field>
               </v-col>
-              <v-col cols="6">
+              <v-col cols="12" sm="6">
                 <div class="text-subtitle-1 text-medium-emphasis">Last Name</div>
                 <v-text-field density="compact" v-model="registerFormData.last_name" placeholder="Last Name"
                   prepend-inner-icon="mdi-account-outline" variant="outlined"></v-text-field>
@@ -136,6 +131,10 @@ export default {
       email: "",
       password: ""
     },
+    cleanLoginData: {
+      username: "",
+      password: "",
+    },
     errors: []
   }),
   methods: {
@@ -203,15 +202,119 @@ export default {
           })
         }
       }
-    }
+    },
+    async login() {
+      this.errors = [];
+
+      // Trim whitespace from input fields
+      this.cleanLoginData.username = this.cleanLoginData.username.trim();
+      this.cleanLoginData.password = this.cleanLoginData.password.trim();
+
+      // Input validation
+      if (
+        !this.cleanLoginData.username ||
+        !this.cleanLoginData.password
+      ) {
+        toast("Please enter your email/username and password.", {
+          "theme": "colored",
+          "type": "error",
+          "dangerouslyHTMLString": true
+        });
+        return;
+      }
+
+      try {
+        let response;
+        response = await axios.post('http://127.0.0.1:8000/api/user/login', this.cleanLoginData);
+        console.log(response.data); // Handle successful response
+        // Redirect or show a success message
+      } catch (error) {
+        const errorMessage = error.response ? error.response.data.error : error.message;
+        if (errorMessage) {
+          toast(errorMessage, {
+            "theme": "colored",
+            "type": "error",
+            "dangerouslyHTMLString": true
+          });
+        } else {
+          // If error message is empty or undefined, display a generic error message
+          toast("An error occurred. Please try again later.", {
+            "theme": "colored",
+            "type": "error",
+            "dangerouslyHTMLString": true
+          });
+        }
+      }
+    },
   }
 }
 </script>
 
 <style>
+.app-overlay {
+  overflow-x: hidden;
+  overflow-y: auto;
+  height: 100% !important;
+  width: 100% !important;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
 .app-login-close-icon {
   position: absolute;
   top: -51px;
   right: -43px;
+}
+
+@media (max-width:595px) {
+  #login .login-card {
+    max-width: 448px !important;
+  }
+
+
+}
+
+@media (max-width:800px) {
+  #register .register-card {
+    max-width: 448px !important;
+  }
+
+  .app-login-close-icon {
+    right: 50%;
+    transform: translateX(50%);
+  }
+}
+
+@media (max-width:470px) {
+  #login .login-card {
+    max-width: 400px !important;
+  }
+
+  #register .register-card {
+    max-width: 400px !important;
+  }
+}
+
+@media (max-width:420px) {
+  #login .login-card {
+    max-width: 370px !important;
+  }
+
+  #register .register-card {
+    max-width: 370px !important;
+  }
+}
+
+
+@media (max-width:378px) {
+  #login .login-card {
+    max-width: 320px !important;
+  }
+
+  #register .register-card {
+    max-width: 320px !important;
+  }
 }
 </style>
